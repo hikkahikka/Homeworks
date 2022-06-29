91d3ea87220fe7cfae795a9e3c7be3d3908494cf
hikkahikka <93731664+hikkahikka@users.noreply.github.com>
2022-06-30 02:45:56 +0300

    nachal dostavku
    silno tuplu

---
+++ HOMEWORK/dostavka/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dostavka
+{
+    public  class Food
+    {
+        /// <summary>
+        /// массивы с вариантами еды
+        /// </summary>
+        private static readonly string[,] BREAKFAST = { { "Scrambled", "100", "260" }, {"Eggs toast","70","320" }, { "Porridge", "180", "125" },{ "Sausages" , "120", "280"} };//имя цена ккал
+        private static readonly string[,] SECOND_BREAKFAST = { { "Fruits", "110", "70" }, { "Sandwich", "120", "155" }, { "Nuts", "180", "100" }, { "Cottage cheese", "100", "240" } };
+        private static readonly string[,] LUNCH = { { "Borscht", "240", "100" }, { "Noodles", "200", "140" }, { "Pasta with cutlet", "210", "" }, { "Shawarma", "220", "500" } };
+        private static readonly string[,] SNACK = { { "Sandwich", "120", "155" }, { "Ice cream", "100", "200" }, { "Yogurt", "70", "60" }, { "Chocopai", "40", "129" } };
+        private static readonly string[,] DINNER = { { "Steak with rice", "390", "400" }, { "Potatoes with mushrooms", "270", "180" }, { "Strips", "170", "250" }, { "Sushi", "230", "270" } };
+
+        
+
+        //получить калораж, сумму, 
+
+    }
+}
